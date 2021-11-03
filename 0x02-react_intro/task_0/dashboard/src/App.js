aa4242f2d3dda8ce7010f44d4f2ad2ae87d3febf
Tim <timmcmackenjr@gmail.com>
2021-11-03 11:44:20 -0400

    task zero

--- 0x02-react_intro/task_0/dashboard/src/App.js
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './HolbertonLogo.jpeg';
 import './App.css';
 
 function App() {
@@ -6,18 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>School dashboard</h1>
       </header>
+      <hr className="HR-red"></hr>
+      <body className="App-body">
+        <p className="Body-p">Login to access the full dashboard</p>
+      </body>
+      <hr className="HR-red"></hr>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
     </div>
   );
 }
