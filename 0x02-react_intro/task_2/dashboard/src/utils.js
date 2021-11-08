ca1399d9850081e885ec7a9912056c6d9fc9a2a2
TMcMac <timmcmackenjr@gmail.com>
2021-11-08 21:56:28 +0000

    Adds more for task 3

--- 0x02-react_intro/task_2/dashboard/src/utils.js
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -5,3 +5,6 @@ export const getFooterCopy = (isIndex) => {
     if (isIndex) return 'Holberton School'
     else return 'Holberton School main dashboard'
 }
+export const getLatestNotification = () => { 
+    return '<strong>Urgent requirement</strong> - complete by EOD'
+}
