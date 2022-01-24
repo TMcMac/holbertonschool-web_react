f05c4f57784bed385e56df59aa029c9bc31d7a64
Tim <timmcmackenjr@gmail.com>
2022-01-23 19:59:22 -0600

    Readding files

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -1,46 +1,32 @@
-// create a prototype for a student
 interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
+	  firstName: string;
+	  lastName: string;
+	  age: number;
+	  location: string;
 }
 
-// create two students
-const student1: Student = {
-  firstName: 'Elliott',
-  lastName: 'Werner',
-  age: 1,
-  location: 'Tulsa'
-};
-let student2: Student = {
-  firstName: 'TBone',
-  lastName: 'Tanner',
-  age: 42,
-  location: 'Las Cruces'
-};
+const student1: Student  = {
+      firstName: 'Suze',
+      lastName: 'Q',
+      age: 42,
+      location: 'Yorba Linda, CA'
+}
 
-// insert students into a students array
-const studentsList: Array<Student> = [student1, student2];
+const student2: Student = {
+      firstName: 'Buzz',
+      lastName: 'Lightyear',
+      age: 1427,
+      location: 'Zurton'
+}
 
-// create a table in vanilla js to display students
-const table: HTMLTableElement = document.createElement('table');
-
-// create table header titles
-const thead: HTMLTableSectionElement = table.createTHead();
-const titleRow: HTMLTableRowElement = table.insertRow();
-const titleName: HTMLTableCellElement = titleRow.insertCell();
-const titleLoc: HTMLTableCellElement = titleRow.insertCell();
-titleName.innerHTML = 'Name';
-titleLoc.innerHTML = 'Location';
-
-// create body and append student data to table
-const tbody: HTMLTableSectionElement = table.createTBody();
-studentsList.forEach(function (student) {
+const studentsList: Array<Student> = [student1, student2]
+const table: HTMLTableElement = document.createElement('table')
+const tableBod: HTMLTableSectionElement = table.createTBody();
+studentsList.forEach(function(student) {
   const newRow: HTMLTableRowElement = table.insertRow();
-  const newName: HTMLTableCellElement = newRow.insertCell();
-  const newLocation: HTMLTableCellElement = newRow.insertCell();
-  newName.innerHTML = student.firstName;
-  newLocation.innerHTML = student.location;
-});
+  const newFirst: HTMLTableCellElement = newRow.insertCell();
+  const newLoc: HTMLTableCellElement = newRow.insertCell();
+  newFirst.innerHTML = student.firstName;
+  newLoc.innerHTML = student.location;
+  });
 document.body.appendChild(table);
